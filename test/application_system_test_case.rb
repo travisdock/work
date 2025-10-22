efce09c0cec8568994857d560ae0b8f582d7c26f
Travis Dockter <travisdock@gmail.com>
2025-10-22 06:10:10 -0600

    Fix system tests to work with authentication

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include SystemSessionTestHelper
+
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_option|
     driver_option.add_argument("--no-sandbox")
     driver_option.add_argument("--disable-dev-shm-usage")
@@ -8,4 +10,14 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     driver_option.add_argument("--remote-debugging-port=0")
     driver_option.add_argument("--user-data-dir=/tmp/chrome_test_#{Process.pid}_#{Time.now.to_i}")
   end
+
+  setup do
+    sign_in_as(system_test_user) if system_test_user
+  end
+
+  private
+
+    def system_test_user
+      users(:one)
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,10 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require_relative "test_helpers/session_test_helper"
+require_relative "test_helpers/system_session_test_helper"
 
 module ActiveSupport
   class TestCase
-    include SessionTestHelper
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
@@ -14,4 +14,8 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
   end
+end
+
+class ActionDispatch::IntegrationTest
+  include SessionTestHelper
 end
