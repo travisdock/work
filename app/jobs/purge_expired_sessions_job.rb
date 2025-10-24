4feec0cf48b085f6464f0f3742dc6dcb067263c5
Travis Dockter <travisdock@gmail.com>
2025-10-24 06:03:44 -0600

    Simplify job

--- app/jobs/purge_expired_sessions_job.rb
+++ app/jobs/purge_expired_sessions_job.rb
@@ -2,10 +2,6 @@ class PurgeExpiredSessionsJob < ApplicationJob
   queue_as :default
 
   def perform
-    now = Time.current
-    inactivity_cutoff = Session::INACTIVITY_TIMEOUT.ago
-
-    Session.where("expires_at <= ?", now).delete_all
-    Session.where("last_activity_at <= ?", inactivity_cutoff).delete_all
+    Session.expired.delete_all
   end
 end
